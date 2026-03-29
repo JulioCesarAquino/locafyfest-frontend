dc8c34cc5f702da9bc47f15f5eaf96457dd626e9
Julio Cesar Lopes de Aquino <julio.aquino@pecararabr.com.br>
2026-03-29 06:05:43 -0300

    fix: update base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: '/locafyfest-frontend/',
+  base: '/',
   server: {
     host: "0.0.0.0",
     port: 8080,
